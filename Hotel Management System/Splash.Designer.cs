623d7fb08682b2f7ee652f08a450c84d9926d6e4
jviaje <jeraldviaje2002@gmail.com>
2022-10-30 08:14:49 +0800

    Add project files.

---
+++ Hotel Management System/Splash.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Hotel_Management_System
+{
+    partial class Splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
+			this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+			this.guna2CircleButton2 = new Guna.UI2.WinForms.Guna2CircleButton();
+			this.guna2CircleButton3 = new Guna.UI2.WinForms.Guna2CircleButton();
+			this.guna2CircleButton4 = new Guna.UI2.WinForms.Guna2CircleButton();
+			this.guna2CircleButton5 = new Guna.UI2.WinForms.Guna2CircleButton();
+			this.myprogressBar = new System.Windows.Forms.ProgressBar();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// guna2CircleButton1
+			// 
+			this.guna2CircleButton1.CheckedState.Parent = this.guna2CircleButton1;
+			this.guna2CircleButton1.CustomImages.Parent = this.guna2CircleButton1;
+			this.guna2CircleButton1.FillColor = System.Drawing.Color.Green;
+			this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+			this.guna2CircleButton1.HoverState.Parent = this.guna2CircleButton1;
+			this.guna2CircleButton1.Location = new System.Drawing.Point(25, 303);
+			this.guna2CircleButton1.Name = "guna2CircleButton1";
+			this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+			this.guna2CircleButton1.ShadowDecoration.Parent = this.guna2CircleButton1;
+			this.guna2CircleButton1.Size = new System.Drawing.Size(148, 148);
+			this.guna2CircleButton1.TabIndex = 0;
+			// 
+			// guna2CircleButton2
+			// 
+			this.guna2CircleButton2.CheckedState.Parent = this.guna2CircleButton2;
+			this.guna2CircleButton2.CustomImages.Parent = this.guna2CircleButton2;
+			this.guna2CircleButton2.FillColor = System.Drawing.Color.Red;
+			this.guna2CircleButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2CircleButton2.ForeColor = System.Drawing.Color.White;
+			this.guna2CircleButton2.HoverState.Parent = this.guna2CircleButton2;
+			this.guna2CircleButton2.Location = new System.Drawing.Point(352, 332);
+			this.guna2CircleButton2.Name = "guna2CircleButton2";
+			this.guna2CircleButton2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+			this.guna2CircleButton2.ShadowDecoration.Parent = this.guna2CircleButton2;
+			this.guna2CircleButton2.Size = new System.Drawing.Size(148, 148);
+			this.guna2CircleButton2.TabIndex = 0;
+			// 
+			// guna2CircleButton3
+			// 
+			this.guna2CircleButton3.CheckedState.Parent = this.guna2CircleButton3;
+			this.guna2CircleButton3.CustomImages.Parent = this.guna2CircleButton3;
+			this.guna2CircleButton3.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2CircleButton3.ForeColor = System.Drawing.Color.White;
+			this.guna2CircleButton3.HoverState.Parent = this.guna2CircleButton3;
+			this.guna2CircleButton3.Location = new System.Drawing.Point(624, -19);
+			this.guna2CircleButton3.Name = "guna2CircleButton3";
+			this.guna2CircleButton3.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+			this.guna2CircleButton3.ShadowDecoration.Parent = this.guna2CircleButton3;
+			this.guna2CircleButton3.Size = new System.Drawing.Size(148, 148);
+			this.guna2CircleButton3.TabIndex = 0;
+			// 
+			// guna2CircleButton4
+			// 
+			this.guna2CircleButton4.CheckedState.Parent = this.guna2CircleButton4;
+			this.guna2CircleButton4.CustomImages.Parent = this.guna2CircleButton4;
+			this.guna2CircleButton4.FillColor = System.Drawing.Color.Orange;
+			this.guna2CircleButton4.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2CircleButton4.ForeColor = System.Drawing.Color.White;
+			this.guna2CircleButton4.HoverState.Parent = this.guna2CircleButton4;
+			this.guna2CircleButton4.Location = new System.Drawing.Point(-33, -57);
+			this.guna2CircleButton4.Name = "guna2CircleButton4";
+			this.guna2CircleButton4.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+			this.guna2CircleButton4.ShadowDecoration.Parent = this.guna2CircleButton4;
+			this.guna2CircleButton4.Size = new System.Drawing.Size(148, 148);
+			this.guna2CircleButton4.TabIndex = 0;
+			// 
+			// guna2CircleButton5
+			// 
+			this.guna2CircleButton5.CheckedState.Parent = this.guna2CircleButton5;
+			this.guna2CircleButton5.CustomImages.Parent = this.guna2CircleButton5;
+			this.guna2CircleButton5.FillColor = System.Drawing.Color.Blue;
+			this.guna2CircleButton5.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2CircleButton5.ForeColor = System.Drawing.Color.White;
+			this.guna2CircleButton5.HoverState.Parent = this.guna2CircleButton5;
+			this.guna2CircleButton5.Location = new System.Drawing.Point(269, -129);
+			this.guna2CircleButton5.Name = "guna2CircleButton5";
+			this.guna2CircleButton5.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+			this.guna2CircleButton5.ShadowDecoration.Parent = this.guna2CircleButton5;
+			this.guna2CircleButton5.Size = new System.Drawing.Size(148, 148);
+			this.guna2CircleButton5.TabIndex = 0;
+			// 
+			// myprogressBar
+			// 
+			this.myprogressBar.Location = new System.Drawing.Point(-1, 343);
+			this.myprogressBar.Name = "myprogressBar";
+			this.myprogressBar.Size = new System.Drawing.Size(700, 11);
+			this.myprogressBar.TabIndex = 2;
+			// 
+			// timer
+			// 
+			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(286, 54);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(119, 125);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Sitka Display", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.label2.Location = new System.Drawing.Point(199, 182);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(301, 47);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Management System";
+			// 
+			// Splash
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.ClientSize = new System.Drawing.Size(700, 350);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.myprogressBar);
+			this.Controls.Add(this.guna2CircleButton5);
+			this.Controls.Add(this.guna2CircleButton4);
+			this.Controls.Add(this.guna2CircleButton3);
+			this.Controls.Add(this.guna2CircleButton2);
+			this.Controls.Add(this.guna2CircleButton1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Splash";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Splash";
+			this.Load += new System.EventHandler(this.Splash_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton2;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton3;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton4;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton5;
+        private System.Windows.Forms.ProgressBar myprogressBar;
+        private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label2;
+	}
+}
